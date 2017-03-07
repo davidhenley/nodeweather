b79f215a912e14cfea89fd7fa50c21eaffad4091
davidhenley <henleydr@gmail.com>
2017-03-07 16:05:42 -0600

    Logging address info from Google Maps API

--- index.js
+++ index.js
@@ -4,5 +4,7 @@ request({
   url: `http://maps.googleapis.com/maps/api/geocode/json?address=7337%20cockrill%20bend%20blvd%20nashville%20tn%2037209`,
   json: true
 }, (err, res, body) => {
-  console.log(body);
+  console.log(`Address: ${body.results[0].formatted_address}`);
+  console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+  console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
 });
