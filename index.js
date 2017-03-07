1f7851a0d37c54e5915b2915929d3ccef6004af4
davidhenley <henleydr@gmail.com>
2017-03-07 16:40:34 -0600

    Error handling

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const { argv } = yargs
+  .options({
+    address: {
+      demand: true,
+      alias: 'a',
+      describe: 'Address to fetch weather for',
+      string: true
+    }
+  })
+  .help()
+  .alias('help', 'h');
+
+const address = encodeURIComponent(argv.address);
 
 request({
-  url: `http://maps.googleapis.com/maps/api/geocode/json?address=7337%20cockrill%20bend%20blvd%20nashville%20tn%2037209`,
+  url: `http://maps.googleapis.com/maps/api/geocode/json?address=${address}`,
   json: true
 }, (err, res, body) => {
-  console.log(`Address: ${body.results[0].formatted_address}`);
-  console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-  console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+  if (err) console.log('Error fetching results.');
+  else if (body.status === 'ZERO_RESULTS') console.log('No results found.');
+  else if (body.status === 'OK') {
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+  }
 });
