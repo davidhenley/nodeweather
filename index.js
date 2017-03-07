3e5ede808f8d92caf4d52c05f1054d7c7693c0de
davidhenley <henleydr@gmail.com>
2017-03-07 17:13:46 -0600

    Using Dark Sky API

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 const yargs = require('yargs');
 
 const geocode = require('./geocode/geocode');
@@ -17,4 +18,23 @@ const { argv } = yargs
 geocode.geocodeAddress(argv.address, (err, res) => {
   if (err) console.log(err);
   else console.log(JSON.stringify(res, null, 2));
+});
+*/
+
+// d3c0b04e02707d2c3d8f0caa454f7cc0
+// https://api.darksky.net/forecast/API_KEY/LATITUDE,LONGITUDE
+
+const request = require('request');
+
+const API_KEY = 'd3c0b04e02707d2c3d8f0caa454f7cc0';
+const LATITUDE = '36.0923299';
+const LONGITUDE = '-86.80333';
+const ROOT_URL = `https://api.darksky.net/forecast/${API_KEY}/${LATITUDE},${LONGITUDE}`;
+
+request({
+  url: ROOT_URL,
+  json: true
+}, (err, res, body) => {
+  if (!err && res.statusCode === 200) console.log(body.currently.temperature);
+  else console.log('Unable to fetch weather');
 });
