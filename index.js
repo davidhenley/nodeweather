a130281eab6707c97b81903b54d711b1db4aaf71
davidhenley <henleydr@gmail.com>
2017-03-07 16:54:48 -0600

    Refactoring to use callbacks

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const request = require('request');
 const yargs = require('yargs');
+
+const geocode = require('./geocode/geocode');
 
 const { argv } = yargs
   .options({
@@ -13,17 +14,7 @@ const { argv } = yargs
   .help()
   .alias('help', 'h');
 
-const address = encodeURIComponent(argv.address);
-
-request({
-  url: `http://maps.googleapis.com/maps/api/geocode/json?address=${address}`,
-  json: true
-}, (err, res, body) => {
-  if (err) console.log('Error fetching results.');
-  else if (body.status === 'ZERO_RESULTS') console.log('No results found.');
-  else if (body.status === 'OK') {
-    console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
-  }
+geocode.geocodeAddress(argv.address, (err, res) => {
+  if (err) console.log(err);
+  else console.log(JSON.stringify(res, null, 2));
 });
